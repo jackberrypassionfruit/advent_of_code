44ad48c49d9aa2ffad0cb16b0bbb961f15e27c14
jackberrypassionfruit <pashayanjack@gmail.com>
2023-11-27 22:40:41 -0500

    finished day_08 part 1

--- day_08/visible_trees.py
+++ day_08/visible_trees.py
@@ -13,8 +13,16 @@ for col in range(width):
   cols.append([row[col] for row in lines])
 # print(cols)
 
-for i in range(width):
-  for j in range(width):
-    pass
-    # TODO
-    # check if the given tree is the max of the trees above, below, left, and right of it
+vis_trees = (width * 4) - 4
+for i in range(1, width - 1):
+  for j in range(1, width - 1):
+    left = rows[j][:i]
+    right = rows[j][i+1:]
+    above = cols[i][:j]
+    below = cols[i][j+1:]
+    num = rows[j][i]
+    if num > max(left) or num > max(right) or num > max(above) or num > max(below):
+      # print(f'i: {i},\nj: {j},\nnum: {num},\nleft: {left},\nright: {right},\nabove: {above},\nbelow: {below}\n\n')
+      vis_trees += 1
+      
+print(vis_trees)
