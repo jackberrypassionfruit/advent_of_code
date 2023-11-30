9183214db1a3cf8c8a9ee791ffc88dc17f54d417
jackberrypassionfruit <pashayanjack@gmail.com>
2023-11-29 23:47:22 -0500

    so close to day_09 p2, having a problem where knot getting pulled allow has a coord with a decimal, and thus is not real

--- day_09/rope_bridge.py
+++ day_09/rope_bridge.py
@@ -1,6 +1,4 @@
-
-
-class RopePhysics():
+class RopePhysicsShort():
   
   def _calculate_board_size(self, actions):
     x, y, max_x, max_y = 0, 0, 0, 0
@@ -29,9 +27,6 @@ class RopePhysics():
     
     self.actions = actions
     self.max_x, self.max_y = self._calculate_board_size(actions)
-      
-  def __repr__(self):
-    return f'head_x: {self.head_x}, head_y: {self.head_y}'
   
   def draw_board(self, show_path=False):
     head_y = self.max_y - self.head_y - 1
@@ -39,7 +34,7 @@ class RopePhysics():
     board = ''
     for y in range(self.max_y):
       for x in range(self.max_x):
-        if show_path and (x, y) in self.tails_travels:
+        if show_path and [x, y] in self.tails_travels:
           board += '#'
         elif not show_path and x == self.head_x and y == head_y:
           board += 'H'
@@ -49,8 +44,6 @@ class RopePhysics():
           board += '.'
       board += '\n'
     print(board)
-    # if show_path:
-    #   print(self.tails_travels)
     
   def _tail_follow_head(self):
     x_diff = self.head_x - self.tail_x
@@ -81,6 +74,100 @@ class RopePhysics():
       dir, dist = action
       for i in range(dist):
         self.move_head_one_space(dir)
+        
+  def get_tail_path_count(self):
+    return len(self.tails_travels)
+  
+class RopePhysicsLong():
+  
+  def _calculate_board_size(self, actions):
+    x, y, max_x, max_y, min_x, min_y = 0, 0, 0, 0, 0, 0
+    for action in actions:
+      dir, dist = action
+      if   dir == 'R':
+        x += dist
+        max_x = max(x, max_x)
+      elif dir == 'L':
+        x -= dist
+        min_x = min(x, min_x)
+      elif dir == 'U':
+        y += dist
+        max_y = max(y, max_y)
+      elif dir == 'D':
+        y -= dist
+        min_y = min(y, min_y)
+    return max_x+1, max_y+1, min_x, min_y
+  
+  def __init__(self, actions):    
+    self.knots = [[0, 0] for i in range(10)]
+    self.tails_travels = set()
+    
+    self.actions = actions
+    self.max_x, self.max_y, self.min_x, self.min_y = self._calculate_board_size(actions)
+  
+  def draw_board(self, show_path=False):
+    mirror_knots = [[knot[0], self.max_y + self.min_y - knot[1] - 1] for knot in self.knots]
+    board = ''
+    for y in range(self.min_y, self.max_y):
+      for x in range(self.min_x, self.max_x):
+        if show_path and tuple([x, y]) in self.tails_travels:
+          board += '#'
+        elif  not show_path and [x, y] == mirror_knots[9]:
+          board += 'H'
+        elif not show_path and [x, y] in mirror_knots:
+          for knot_index in range(len(mirror_knots) - 2 , -1, -1): # range(8 -> 0)
+            if [x, y] == mirror_knots[knot_index]:
+              board += str(knot_index + 1)
+              break
+        else:
+          board += '.'
+      board += '\n'
+    print(board)
+    
+  def move_head(self, dir):
+    match dir:
+      case 'R':
+        self.knots[9][0] += 1
+      case 'L':
+        self.knots[9][0] -= 1
+      case 'U':
+        self.knots[9][1] += 1
+      case 'D':
+        self.knots[9][1] -= 1
+    
+  def all_knots_follow(self):
+    for knot_index in range(len(self.knots) - 2 , -1, -1): # range(8 -> 0)
+      lo_knot = self.knots[knot_index]
+      hi_knot = self.knots[knot_index + 1]
+      
+      x_diff = hi_knot[0] - lo_knot[0]
+      y_diff = hi_knot[1] - lo_knot[1]
+      
+      # Something odd here is causing one of the knots to not get carried along in a diagnonal
+      # for some reason, its new y becomes a fraction, adn then everyting is fucked up
+      if   abs(x_diff) > 1:
+        lo_knot[0] += (x_diff / 2)
+        lo_knot[1] = hi_knot[1]
+      elif abs(y_diff) > 1:
+        lo_knot[1] += (y_diff / 2)
+        lo_knot[0] = hi_knot[0]
+        
+      self.knots[knot_index] = lo_knot
+      self.knots[knot_index + 1] = hi_knot
+        
+  def make_step(self, dir):
+    self.move_head(dir)
+    self.all_knots_follow()
+    self.tails_travels.update([(self.knots[0][0], self.max_y + self.min_y-self.knots[0][1]-1)])
+    self.draw_board()
+    print(self.knots)
+    
+    
+  def perform_actions(self):
+    for action in self.actions:
+      dir, dist = action
+      for i in range(dist):
+        self.make_step(dir)
         
   def get_tail_path_count(self):
     return len(self.tails_travels)
--- day_09/test.py
+++ day_09/test.py
@@ -1,4 +1,4 @@
-from rope_bridge import RopePhysics
+from rope_bridge import *
 import sys, json
 
 with open (sys.argv[1], 'r') as infile:
@@ -8,12 +8,27 @@ actions = [(line.split(' ')[0], int(line.split(' ')[1])) for line in lines]
 # for action in actions:
 #   print(action)
 
-rope_physics = RopePhysics(actions)
-# rope_physics._calculate_board_size(actions)
-# rope_physics.draw_board()
-rope_physics.perform_actions()
+# rope_physics_short = RopePhysicsShort(actions)
+# # rope_physics_short._calculate_board_size(actions)
+# # rope_physics_short.draw_board()
+# rope_physics_short.perform_actions()
 
 
 
-# rope_physics.draw_board(show_path=True)
-print(rope_physics.get_tail_path_count())
+# # rope_physics_short.draw_board(show_path=True)
+# print(rope_physics_short.get_tail_path_count())
+
+
+rope_physics_long = RopePhysicsLong(actions)
+rope_physics_long.perform_actions()
+rope_physics_long.draw_board(show_path=True)
+print(rope_physics_long.get_tail_path_count())
+# print(rope_physics_long._calculate_board_size(actions))
+
+
+# test = range(5, 0, -1)
+# for i in test:
+#   print(i)
+
+
+### answer is SMALLER THAN 2561
