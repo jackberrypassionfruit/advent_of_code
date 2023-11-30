ebb7ad34477cc8f1ffb63536cd8f1b9abf957794
jackberrypassionfruit <pashayanjack@gmail.com>
2023-11-29 20:53:50 -0500

    finished day_09 p1

--- day_09/rope_bridge.py
+++ day_09/rope_bridge.py
@@ -1,19 +1,12 @@
 
 
 class RopePhysics():
-  def __init__(self, actions):
-    self.tail_x = 0
-    self.tail_y = 0
-    self.head_x = 0
-    self.head_y = 0
-    
-    self.tails_travels = set()
-    
-    self.actions = actions
+  
+  def _calculate_board_size(self, actions):
     x, y, max_x, max_y = 0, 0, 0, 0
     for action in actions:
       dir, dist = action
-      if dir == 'R':
+      if   dir == 'R':
         x += dist
         max_x = max(x, max_x)
       elif dir == 'L':
@@ -23,12 +16,51 @@ class RopePhysics():
         max_y = max(y, max_y)
       elif dir == 'D':
         y -= dist
+      # print(f'x: {x}, y: {y}, max_x: {max_x}, max_y: {max_y}')
+    return max_x+1, max_y+1
+  
+  def __init__(self, actions):
+    self.tail_x = 0
+    self.tail_y = 0
+    self.head_x = 0
+    self.head_y = 0
     
-    self.max_x = max_x
-    self.max_y = max_y
+    self.tails_travels = set()
+    
+    self.actions = actions
+    self.max_x, self.max_y = self._calculate_board_size(actions)
       
   def __repr__(self):
     return f'head_x: {self.head_x}, head_y: {self.head_y}'
+  
+  def draw_board(self, show_path=False):
+    head_y = self.max_y - self.head_y - 1
+    tail_y = self.max_y - self.tail_y - 1
+    board = ''
+    for y in range(self.max_y):
+      for x in range(self.max_x):
+        if show_path and (x, y) in self.tails_travels:
+          board += '#'
+        elif not show_path and x == self.head_x and y == head_y:
+          board += 'H'
+        elif not show_path and x == self.tail_x and y == tail_y:
+          board += 'T'
+        else:
+          board += '.'
+      board += '\n'
+    print(board)
+    # if show_path:
+    #   print(self.tails_travels)
+    
+  def _tail_follow_head(self):
+    x_diff = self.head_x - self.tail_x
+    y_diff = self.head_y - self.tail_y
+    if   abs(x_diff) > 1:
+      self.tail_x += (x_diff / 2)
+      self.tail_y = self.head_y
+    elif abs(y_diff) > 1:
+      self.tail_y += (y_diff / 2)
+      self.tail_x = self.head_x
     
   def move_head_one_space(self, dir):
     if   dir == 'R':
@@ -38,21 +70,17 @@ class RopePhysics():
     elif dir == 'U':
       self.head_y += 1
     elif dir == 'D':
-      self.head_y += 1
-    # TODO
-    # Add functionality to make the tail trail behind the head
+      self.head_y -= 1
       
-  def draw_board(self):
-    head_y = self.max_y - self.head_y - 1
-    tail_y = self.max_y - self.tail_y - 1
-    board = ''
-    for y in range(self.max_y):
-      for x in range(self.max_x):
-        if   x == self.head_x and y == head_y:
-          board += 'H'
-        elif x == self.tail_x and y == tail_y:
-          board += 'T'
-        else:
-          board += '.'
-      board += '\n'
-    print(board)
+    self._tail_follow_head()
+    self.tails_travels.update([(self.tail_x, self.max_y-self.tail_y-1)])
+    # self.draw_board()
+    
+  def perform_actions(self):
+    for action in self.actions:
+      dir, dist = action
+      for i in range(dist):
+        self.move_head_one_space(dir)
+        
+  def get_tail_path_count(self):
+    return len(self.tails_travels)
--- day_09/test.py
+++ day_09/test.py
@@ -1,5 +1,4 @@
 from rope_bridge import RopePhysics
-
 import sys, json
 
 with open (sys.argv[1], 'r') as infile:
@@ -10,8 +9,11 @@ actions = [(line.split(' ')[0], int(line.split(' ')[1])) for line in lines]
 #   print(action)
 
 rope_physics = RopePhysics(actions)
-rope_physics.move_head_one_space('U')
-rope_physics.move_head_one_space('R')
-# # print(rope_physics)
+# rope_physics._calculate_board_size(actions)
+# rope_physics.draw_board()
+rope_physics.perform_actions()
 
-rope_physics.draw_board()
+
+
+# rope_physics.draw_board(show_path=True)
+print(rope_physics.get_tail_path_count())
