5209b9417e3fb195a814481d8c1699ae2998c2d0
jackberrypassionfruit <pashayanjack@gmail.com>
2023-12-09 12:38:41 -0500

    all but solved day_15 p1

--- day_15/beacon_sweeper.py
+++ day_15/beacon_sweeper.py
@@ -23,12 +23,6 @@ class BeaconExclusion():
       x_sensor, y_sensor = [int(num) for num in sensor_line.split(', y=')]
       beacon_line = beacon_line.lstrip('closest beacon is at x=')
       x_beacon, y_beacon = [int(num) for num in beacon_line.split(', y=')]
-      # TODO
-      # THis needs to be done after all of the exclusion zones are calculated
-      self.max_x = max(self.max_x, x_sensor, x_beacon)
-      self.max_y = max(self.max_y, y_sensor, y_beacon)
-      self.min_x = min(self.min_x, x_sensor, x_beacon)
-      self.min_y = min(self.min_y, y_sensor, y_beacon)
       
       self.beacons.add((x_beacon, y_beacon))
       
@@ -36,11 +30,8 @@ class BeaconExclusion():
       self.sensors.add(Sensor((x_sensor, y_sensor), sensor_exclusion_radius))
       
     self.sensor_coords = [sensor.coords for sensor in self.sensors]
-    
-    
-    #TODO
-    # This part needs to change
-    # def tunnel() needs find a way to use the exclusion zones and their effects on the coordinate bounds, so that it draws a plyfield that incorporaes those
+  
+  def model_tunnel(self):
     self.tunnel = []
     for j in range(self.min_y, self.max_y + 1):
       line = []
@@ -60,28 +51,36 @@ class BeaconExclusion():
       render_tunnel += ''.join(line) + '\n'
     return render_tunnel
     # return str(self.beacons)
-
-  def exclude_zones(self):
-    for sensor in list(self.sensors)[:]:
+    
+  def calculate_tunnel_bounds(self):
+    for sensor in self.sensors:
       coords, radius = sensor.coords, sensor.exclusion_radius
       x_coord, y_coord = [int(coord) for coord in coords]
-      print(coords)
       for delta_y in range(-1*radius, radius + 1):
         for delta_x in range(-1*(radius - abs(delta_y)) , (radius - abs(delta_y)+1)):
           current_x = x_coord + delta_x
           current_y = y_coord + delta_y
-          # if current_y >= self.min_y and current_y <= self.max_y and current_x >= self.min_x and current_x <= self.max_x: 
-            current_coords_value = self.tunnel[current_y][current_x - self.min_x]
-            if current_coords_value not in ['S', 'B', '#']:
-              self.tunnel[current_y][current_x - self.min_x] = '#'
-            
-        #   print('X', end='')
-        # print()
+          self.max_x = max(self.max_x, current_x)
+          self.min_x = min(self.min_x, current_x)
+          self.max_y = max(self.max_y, current_y)
+          self.min_y = min(self.min_y, current_y)
+
+  def exclude_zones(self):    
+    for sensor in list(self.sensors)[:]:
+      coords, radius = sensor.coords, sensor.exclusion_radius
+      x_coord, y_coord = [int(coord) for coord in coords]  
+      for delta_y in range(-1*radius, radius + 1):
+        for delta_x in range(-1*(radius - abs(delta_y)) , (radius - abs(delta_y)+1)):
+          current_x = x_coord + delta_x
+          current_y = y_coord + delta_y
+          current_coords_value = self.tunnel[current_y - self.min_y][current_x - self.min_x]
+          if current_coords_value == '.':
+            self.tunnel[current_y - self.min_y][current_x - self.min_x] = '#'
 
 if __name__ == '__main__':
   beacon_sweeper = BeaconExclusion()
-  # TODO
-  # this next nethod needs to draw a larger playfield
+  beacon_sweeper.calculate_tunnel_bounds()
+  beacon_sweeper.model_tunnel()
   beacon_sweeper.exclude_zones()
   print(beacon_sweeper)
   
