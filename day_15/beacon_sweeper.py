904bc4e131168f189c73e803c745e62fbe5e3ccd
jackberrypassionfruit <pashayanjack@gmail.com>
2023-12-10 17:46:54 -0500

    tried to save tunnel frame more efficiently

--- day_15/beacon_sweeper.py
+++ day_15/beacon_sweeper.py
@@ -35,23 +35,21 @@ class BeaconExclusionNaive():
     for j in range(self.min_y, self.max_y + 1):
       line = []
       for i in range(self.min_x, self.max_x + 1):
-        if   (i, j) in self.beacons:
-          line.append('B')
-        elif (i, j) in self.sensor_coords:
-          line.append('S')
-        else:
-          line.append('.')
+        print(i, j)
+        line.append('.')
       self.tunnel.append(line)
+    # self.tunnel = [['.'] * (self.max_x - self.min_x + 1)] * (self.max_y - self.min_y + 1)
 
-  # I want to split up the above function into 2, but this bottom function is not doing what I want it to do
-  # def model_tunnel(self):
-  #   for j in range(self.min_y, self.max_y + 1):
-  #     for i in range(self.min_x, self.max_x + 1):
-        
-  #       if   (i, j) in self.beacons:
-  #         self.tunnel[j][i] = 'B'
-  #       elif (i, j) in self.sensor_coords:
-  #         self.tunnel[j][i] = 'S'
+  def model_tunnel(self):
+    for j in range(self.max_y - self.min_y + 1):
+      for i in range(self.max_x - self.min_x + 1):
+        print(i, j)
+        # the reason the i's and j's are different is because the beacon's/sensor's coords were stored based on the original layout...
+        # but the full layout based on min/max x/y doesn't account for this
+        if   (i, j) in self.beacons:
+          self.tunnel[j - self.min_y][i - self.min_x] = 'B'
+        elif (i, j) in self.sensor_coords:
+          self.tunnel[j - self.min_y][i - self.min_x] = 'S'
       
     
   def __repr__(self):
@@ -93,7 +91,7 @@ if __name__ == '__main__':
   beacon_sweeper.calculate_tunnel_bounds()
   beacon_sweeper.model_tunnel_frame()
   beacon_sweeper.exclude_zones()
-  # beacon_sweeper.model_tunnel()
+  beacon_sweeper.model_tunnel()
   print(beacon_sweeper)
   print(beacon_sweeper.get_num_exclusions_by_row(10))
   
