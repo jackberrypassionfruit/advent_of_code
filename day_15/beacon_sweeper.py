9a03dd841b561d2a1c6c4539501b0a0a1bc6297f
jackberrypassionfruit <pashayanjack@gmail.com>
2023-12-10 08:11:02 -0500

    solved problem of writing B/S to tunnel AFTER drawing exclusions

--- day_15/beacon_sweeper.py
+++ day_15/beacon_sweeper.py
@@ -35,23 +35,18 @@ class BeaconExclusionNaive():
     for j in range(self.min_y, self.max_y + 1):
       line = []
       for i in range(self.min_x, self.max_x + 1):
-        if   (i, j) in self.beacons:
-          line.append('B')
-        elif (i, j) in self.sensor_coords:
-          line.append('S')
-        else:
-          line.append('.')
+        line.append('.')
       self.tunnel.append(line)
 
-  # I want to split up the above function into 2, but this bottom function is not doing what I want it to do
-  # def model_tunnel(self):
-  #   for j in range(self.min_y, self.max_y + 1):
-  #     for i in range(self.min_x, self.max_x + 1):
-        
-  #       if   (i, j) in self.beacons:
-  #         self.tunnel[j][i] = 'B'
-  #       elif (i, j) in self.sensor_coords:
-  #         self.tunnel[j][i] = 'S'
+  def model_tunnel(self):
+    for j in range(self.max_y - self.min_y + 1):
+      for i in range(self.max_x - self.min_x + 1):
+        # the reason the i's and j's are different is because the beacon's/sensor's coords were stored based on the original layout...
+        # but the full layout based on min/max x/y doesn't account for this
+        if   (i, j) in self.beacons:
+          self.tunnel[j - self.min_y][i - self.min_x] = 'B'
+        elif (i, j) in self.sensor_coords:
+          self.tunnel[j - self.min_y][i - self.min_x] = 'S'
       
     
   def __repr__(self):
@@ -69,7 +64,7 @@ class BeaconExclusionNaive():
       self.min_x = min(self.min_x, x_coord - radius)
       self.max_y = max(self.max_y, y_coord + radius)
       self.min_y = min(self.min_y, y_coord - radius)
-  
+    print('min_y: ', self.min_y, 'min_x: ', self.min_x, 'max_x: ', self.max_x, 'max_y: ', self.max_y)
 
   def exclude_zones(self):    
     for sensor in list(self.sensors)[:]:
@@ -93,7 +88,7 @@ if __name__ == '__main__':
   beacon_sweeper.calculate_tunnel_bounds()
   beacon_sweeper.model_tunnel_frame()
   beacon_sweeper.exclude_zones()
-  # beacon_sweeper.model_tunnel()
+  beacon_sweeper.model_tunnel()
   print(beacon_sweeper)
   print(beacon_sweeper.get_num_exclusions_by_row(10))
   
