79ef78bdd55abd38cfb07092fa9a8c3fa6cb3bae
jackberrypassionfruit <pashayanjack@gmail.com>
2023-12-17 14:45:16 -0500

    day 17 added check collission and stop_block methods

--- day_17/pyroclastic_flow.py
+++ day_17/pyroclastic_flow.py
@@ -1,4 +1,4 @@
-import sys
+import sys, math
 
 class FallingRocks():
   def __init__(self):
@@ -7,30 +7,32 @@ class FallingRocks():
     self.jet_gas = ''.join(jet_push_text.split('\n'))
     
     self.blocks = [
-      ['####'],
+      ['@@@@'],
       [
-        '.#.',
-        '###',
-        '.#.'
+        '.@.',
+        '@@@',
+        '.@.'
       ],
       # this next one is flipped because I'm storing the playfield upside down in memory
       [
-        '###',
-        '..#',
-        '..#'
+        '@@@',
+        '..@',
+        '..@'
       ],
       [ 
-        '#',
-        '#',
-        '#',
-        '#'
+        '@',
+        '@',
+        '@',
+        '@'
       ],
       [
-        '##',
-        '##'
+        '@@',
+        '@@'
       ]
     ]
       
+    # testing
+    self.block_carcasses = set({ (2,0), (3,0), (4,0), (5,0) })
     self.feeld = '''\
 ..####.
 .......
@@ -40,18 +42,20 @@ class FallingRocks():
 '''.split('\n')
 
     self.current_loc = [2,1]
+    self.block_index = 2
+
     
   def __repr__(self):
-    block_index = 4
     result = ''
     for line_index, line in enumerate(self.feeld):
       block_row = line_index - self.current_loc[1]
-      if  0 <= block_row < len(self.blocks[block_index]):
+      # if self.current_loc == [0,0], then yet to drop a new block
+      if self.current_loc != [0,0] and 0 <= block_row < len(self.blocks[self.block_index]):
         block_line = ''
         for col in range(7):
           block_col = col - self.current_loc[0]
-          if self.current_loc[0] <= col <= len(self.blocks[block_index][block_row]) + 1:
-            block_line += self.blocks[block_index][block_row][block_col]
+          if self.current_loc[0] <= col <= len(self.blocks[self.block_index][block_row]) + 1:
+            block_line += self.blocks[self.block_index][block_row][block_col]
           else:
             block_line += '.'
         result += f'{block_line}\n'
@@ -61,9 +65,34 @@ class FallingRocks():
           
           
     # return result
-    return '\n'.join(reversed(result.split('\n'))) # Flip the image upside down
+    return '\n'.join(reversed(result.split('\n'))) # (Un)Flip the image upside down
   
-  def check_if_will_collide(self):
-    raise NotImplementedError
+  def get_current_block_coords(self):  
+    block_coords = set()
+    for block_row_index, block_row in enumerate(self.blocks[self.block_index]):
+      for block_col_index, block_col in enumerate(block_row):
+        if block_col != '.':
+          new_block_spot_x, new_block_spot_y = self.current_loc
+          new_block_spot_x += block_col_index
+          new_block_spot_y += block_row_index
+          block_coords.update([(new_block_spot_x, new_block_spot_y)])
+    return block_coords
     
+  def check_if_will_collide(self):
+    potential_collisions = set()
+    current_block_coords = self.get_current_block_coords()
+    for block_spot in current_block_coords:
+      collision_spot_x, collision_spot_y = block_spot
+      collision_spot_x -= 1
+      collision_spot_y -= 1
+      potential_collisions.update([(collision_spot_x, collision_spot_y)])
+    return potential_collisions.intersection(self.block_carcasses)
   
+  def stop_block(self):
+    block_coords = self.get_current_block_coords()
+    for coords in block_coords:
+      coord_x, coord_y = coords
+      self.feeld[coord_y] = self.feeld[coord_y][:coord_x] + '#' + self.feeld[coord_y][coord_x+1:]
+    self.current_loc = [0,0]
+    #TODO add a return value, max_height, which will be max(current_max_height, max_height_of_this_dead_block)
+    
--- day_17/test.py
+++ day_17/test.py
@@ -1,5 +1,9 @@
 from pyroclastic_flow import FallingRocks
 
 falling_rocks = FallingRocks()
-
+falling_rocks.stop_block()
 print(falling_rocks)
+# if falling_rocks.check_if_will_collide():
+#   print('will collide')
+# else:
+#   print('will not collide')
