82ea29851dc88f149c08fb12f8c27a83a6a68a17
jackberrypassionfruit <pashayanjack@gmail.com>
2023-12-23 18:30:13 -0500

    gave up on day_17

--- day_17/pyroclastic_flow.py
+++ day_17/pyroclastic_flow.py
@@ -1,11 +1,13 @@
 import sys, math
+import time
 
 class FallingRocks():
   def __init__(self):
     with open(sys.argv[1], 'r', encoding='utf8') as infile:
       jet_push_text = infile.read()
-    self.jet_gas = ''.join(jet_push_text.split('\n'))
+    self.hot_gas_movement = list(''.join(jet_push_text.split('\n')))
     
+    self.max_height = -1
     self.blocks = [
       ['@@@@'],
       [
@@ -30,34 +32,33 @@ class FallingRocks():
         '@@'
       ]
     ]
+    self.hot_gas_index = 0
       
-    # testing
-    self.block_carcasses = set({ (2,0), (3,0), (4,0), (5,0) })
     self.feeld = '''\
-..####.
 .......
 .......
 .......
 .......\
 '''.split('\n')
 
-    self.current_loc = [2,1]
-    self.block_index = 2
-
+    self.block_carcasses = set() # set({ (2,0), (3,0), (4,0), (5,0) })
+    # testing
+    # self.max_height = 1
+    # self.current_loc = [2,8]
     
   def __repr__(self):
     result = ''
     for line_index, line in enumerate(self.feeld):
       block_row = line_index - self.current_loc[1]
       # if self.current_loc == [0,0], then yet to drop a new block
-      if self.current_loc != [0,0] and 0 <= block_row < len(self.blocks[self.block_index]):
+      if self.current_loc != [-1, -1] and 0 <= block_row < len(self.blocks[self.block_index]):
         block_line = ''
-        for col in range(7):
-          block_col = col - self.current_loc[0]
-          if self.current_loc[0] <= col <= len(self.blocks[self.block_index][block_row]) + 1:
+        for col_index in range(7):
+          block_col = col_index - self.current_loc[0]
+          if self.current_loc[0] <= col_index < self.current_loc[0] + len(self.blocks[self.block_index][block_row]):
             block_line += self.blocks[self.block_index][block_row][block_col]
           else:
-            block_line += '.'
+            block_line += line[col_index]
         result += f'{block_line}\n'
       else:
         result += f'{line}\n'
@@ -78,21 +79,80 @@ class FallingRocks():
           block_coords.update([(new_block_spot_x, new_block_spot_y)])
     return block_coords
     
-  def check_if_will_collide(self):
+  def check_if_will_collide(self, dir):
     potential_collisions = set()
     current_block_coords = self.get_current_block_coords()
+    # testing 
     for block_spot in current_block_coords:
       collision_spot_x, collision_spot_y = block_spot
-      collision_spot_x -= 1
-      collision_spot_y -= 1
+      match dir:
+        case 'V':
+          collision_spot_y -= 1
+        case '<':
+          collision_spot_x -= 1
+        case '>':
+          collision_spot_x += 1
       potential_collisions.update([(collision_spot_x, collision_spot_y)])
     return potential_collisions.intersection(self.block_carcasses)
   
   def stop_block(self):
+    # This method takes the current state of the block, and turns it into block carcasses
     block_coords = self.get_current_block_coords()
     for coords in block_coords:
       coord_x, coord_y = coords
       self.feeld[coord_y] = self.feeld[coord_y][:coord_x] + '#' + self.feeld[coord_y][coord_x+1:]
-    self.current_loc = [0,0]
-    #TODO add a return value, max_height, which will be max(current_max_height, max_height_of_this_dead_block)
+    self.block_carcasses.update(block_coords)
+    self.current_loc = [-1,-1]
+    # Return a value, max_height, which will be max(current_max_height, max_height_of_this_dead_block)
+    return max(self.max_height, max([block_coord[1] for block_coord in block_coords]))
+  
+  def hot_gas(self, dir):
+    match dir:
+      case '<':
+        if self.current_loc[0] > 0:
+          self.current_loc[0] -= 1
+      case '>':
+        coord_right = self.current_loc[0] + len(self.blocks[self.block_index][0])
+        if coord_right < len(self.feeld[0]):
+          self.current_loc[0] += 1
+        
+  
+  def block_fall_down(self):
+    while self.current_loc != [-1, -1]:
+      # move left or right, depending on wind instruction
+      # BUG does not check block carcasses before moving
+      
+      dir = self.hot_gas_movement[self.hot_gas_index]
+      if not self.check_if_will_collide(dir):
+        self.hot_gas(dir)
+      self.hot_gas_index = (self.hot_gas_index + 1) % len(self.hot_gas_movement)
+      # print('\n'*30)
+      # print(self)
+      # # print('self.current_loc: ', self.current_loc)
+      # # print('dir: ', dir)
+      # print('self.hot_gas_index: ', self.hot_gas_index)
+      # time.sleep(.25)
+      
+      if self.current_loc[1] == 0 or self.check_if_will_collide('V'):
+        self.max_height = self.stop_block()
+      else:
+        self.current_loc[1] -= 1
+      # print('\n'*30)
+      # # print('self.current_loc: ', self.current_loc)
+      # print(self)
+      # print('self.hot_gas_index: ', self.hot_gas_index)
+      # time.sleep(.25)
+      
+  def release_block(self):
+    current_block_height = len(self.blocks[self.block_index])
+    self.feeld += ['.......'] * (current_block_height)
+    self.current_loc = [2, self.max_height + 4]
+    self.block_fall_down()
     
+    
+  def release_the_blocks(self):
+    for i in range(1000000000000):
+      self.block_index = i % len(self.blocks)
+      
+      self.release_block()
+    print('max_height: ', self.max_height)
--- day_17/test.py
+++ day_17/test.py
@@ -1,9 +1,12 @@
 from pyroclastic_flow import FallingRocks
 
 falling_rocks = FallingRocks()
-falling_rocks.stop_block()
-print(falling_rocks)
+# falling_rocks.stop_block()
+# print(falling_rocks)
+
 # if falling_rocks.check_if_will_collide():
 #   print('will collide')
 # else:
 #   print('will not collide')
+# print(len(falling_rocks.hot_gas_movement))
+falling_rocks.release_the_blocks()
